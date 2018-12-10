c5e2eeb86a582ccf5a15f2c872d2ae1fd0a5636b
jperona70 <jacobaperona@gmail.com>
2018-12-10 15:05:13 -0600

    webServer done

--- src/BankClient.java
+++ src/BankClient.java
@@ -10,33 +10,27 @@ public class BankClient
     public static void main(String[] args) throws IOException
     {
         final int SBAP_PORT = 8888;
-        try (Socket s = new Socket("localhost", SBAP_PORT))
-        {
-            System.out.println("Connected");
-
-            InputStream instream = s.getInputStream();
-            OutputStream  outstream = s.getOutputStream();
-            Scanner in = new Scanner(instream);
-            PrintWriter out = new PrintWriter(outstream);
-
-            String command = "Deposit 3 1000\n";
-            System.out.print("Sending" + command);
-            out.print(command);
-            out.flush();
-            String response = in.nextLine();
-            System.out.println("Reciving: " + response);
-
-            command = "Withdraw  3 500\n";
-            System.out.println("Sending: " + command);
-            out.print(command);
-            out.flush();
-            response = in.nextLine();
-            System.out.println("Receiving: " + response);
-
-            command = "QUIT\n";
-            System.out.print("Sending: " + command);
-            out.print(command);
-            out.flush();
-        }
+        Socket s = new Socket("localhost", SBAP_PORT);
+        System.out.println("Connected");
+        //InputStream instream = s.getInputStream();
+        //OutputStream  outstream = s.getOutputStream();
+        //Scanner in = new Scanner(instream);
+        //PrintWriter out = new PrintWriter(outstream);
+        //String command = "Deposit 3 1000\n";
+        //System.out.print("Sending" + command);
+        //out.print(command);
+        //out.flush();
+        //String response = in.nextLine();
+        //System.out.println("Reciving: " + response);
+        //command = "Withdraw  3 500\n";
+        //System.out.println("Sending: " + command);
+        //out.print(command);
+        //out.flush();
+        //response = in.nextLine();
+        //System.out.println("Receiving: " + response);
+        //command = "QUIT\n";
+        //System.out.print("Sending: " + command);
+        //out.print(command);
+        //out.flush();
     }
 }
--- src/BankServer.java
+++ src/BankServer.java
@@ -7,21 +7,19 @@ public class BankServer
 {
     public static void main(String[] args) throws IOException
     {
-        final int ACCOUNTS_LENGTH = 10;
-        Bank bank = new Bank (ACCOUNTS_LENGTH);
+       // final int ACCOUNTS_LENGTH = 10;
+       // Bank bank = new Bank (ACCOUNTS_LENGTH);
         final int SBAP_PORT = 8888;
         ServerSocket server = new ServerSocket(SBAP_PORT);
         System.out.println("Waiting for clients to connect...");
 
         while (true)
         {
-            try (Socket s = server.accept())
-            {
-                System.out.println("Client connected.");
-                BankService service = new BankService(s, bank);
-                Thread t = new Thread(service);
-                t.start();
-            }
+            Socket s = server.accept();
+            System.out.println("Client connected.");
+            //BankService service = new BankService(s, bank);
+            //Thread t = new Thread(service);
+            //t.start();
         }
     }
 }
