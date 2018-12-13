69d7896d74ffcb644970f20d69356aaf781cca04
RoadyC <rchoi@stu.naperville203.org>
2018-12-13 15:06:49 -0600

    please test it

--- Web Chat Server/src/ChatClient.java
+++ Web Chat Server/src/ChatClient.java
@@ -1,3 +1,73 @@
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
 public class ChatClient
 {
+    public static void main(String[] args) throws IOException
+    {
+        final int ChatServer = 8080;
+        try(Socket s = new Socket("localhost", ChatServer))
+        {
+            InputStream instream = s.getInputStream();
+            OutputStream outstream = s.getOutputStream();
+            Scanner in = new Scanner(instream);
+            PrintWriter out = new PrintWriter(outstream);
+            String command;
+            String login;
+            String chat;
+
+            Scanner scn = new Scanner(System.in);
+
+
+            while(true)
+            {
+
+                if(scn.hasNext())
+                {
+                    command = scn.next();
+                    out.print(command);
+                    out.flush();
+                    if(command.equals("LOGIN"))
+                    {
+                        System.out.print("Please Enter your Username: ");
+                        login = scn.next();
+                        out.print(login);
+                        out.flush();
+
+                    }
+                    if(command.equals("LOGOUT"))
+                    {
+                        break;
+                    }
+                    if(command.equals("CHAT"))
+                    {
+                        System.out.println("Enter your message:");
+                        chat = scn.next();
+                        out.println(chat);
+                        out.flush();
+
+                    }
+                }
+
+                if (in.hasNext())
+                {
+                    System.out.println(in);
+
+                }
+
+
+
+            }
+
+            
+
+
+
+        }
+    }
 }
